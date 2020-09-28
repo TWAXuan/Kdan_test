c0ba8c9ef3af04471a38636987c80dccca793bbe
Randy <zzxxccvv484632@gmail.com>
2020-09-28 15:03:11 +0800

    finilly routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'admins#index'
+
+  get :store_scan, to: "features#store_scan"
+  get :books_scan, to: "features#books_scan"
+
+  resources :book, only: [:index, :edit, :update]
+  resources :store, only: [:index, :edit, :update]
+  resources :user, only: [:index, :edit, :update]
+
+  get :user_purchase_login, to: "user_purchase#login"
+  resources :user_purchase, only: [:index, :create]
+  post :user_purchase_check, to: "user_purchase#check"
 end
